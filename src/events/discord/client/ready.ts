41be147d19545b058c904523e4212e750fdf13d5
Peter-MJ-Parker <georgeneedham7@gmail.com>
2024-04-21 02:56:28 +0000

    refactor ready event

--- src/events/discord/client/ready.ts
+++ src/events/discord/client/ready.ts
@@ -1,23 +1,13 @@
 import { fetchNewForms } from "#adapters/Google/reponses";
-import { Paginator } from "#adapters/Paginator";
-import { EventType, Services, eventModule } from "@sern/handler";
-import { Events, TextChannel } from "discord.js";
+import { Services, discordEvent } from "@sern/handler";
+import { Events } from "discord.js";
 
-export default eventModule({
-  type: EventType.Discord,
+export default discordEvent({
   name: Events.ClientReady,
   async execute() {
     const [client, logger] = Services("@sern/client", "@sern/logger");
 
     logger.success(`[CLIENT]- Logged into Discord as ${client.user?.tag}!`);
     await fetchNewForms(2);
-
-
-    // const guilds = Array.from(client.guilds.cache.values());
-
-    // for (const guild of guilds) {
-    //   await guild.commands.set([]);
-    // }
-    // await Service("@sern/client").application?.commands.set([]);
   },
 });
